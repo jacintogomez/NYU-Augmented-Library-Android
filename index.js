e42bf7dc14d743b7bac83b6fc10b7b9a67fabfdb
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-11-06 16:29:56 -0500

    iniital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import WelcomeScreen from "./Screens/WelcomeScreen";
+import Services from "./Screens/Services";
+import About from "./Screens/About";
+import Explore from "./Screens/Explore";
+import ReserveScreen from "./Screens/Reserve Screen/ReserveScreen";
+import PickTime from "./Screens/Reserve Screen/PickTime";
+import ARVision from "./Screens/ARVision";
+import SignIn from "./Screens/SignIn/SignIn";
+import WelcomeButton from "./Screens/components/welcomeComponent/WelcomeButton"
+
+export {
+  WelcomeScreen,
+  Services,
+  About,
+  Explore,
+  ReserveScreen,
+  ARVision,
+  PickTime,
+    SignIn
+}
